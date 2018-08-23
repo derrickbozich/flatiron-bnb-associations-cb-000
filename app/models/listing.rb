66285f6b9833398e74a9497f2c8ef69367f27cd7
derrick b <dbozich1@gmail.com>
2018-08-23 01:08:26 +0000

    automatically backed up by learn

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,6 +4,9 @@ class Listing < ActiveRecord::Base
   belongs_to :neighborhood
   has_many :reservations
   has_many :reviews, through: :reservations
+  
 
   # has_many :guests, :class_name => "User"
+  has_many :guests, through: :reservations
+
 end
