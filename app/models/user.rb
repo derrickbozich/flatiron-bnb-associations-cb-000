e1b654c65a637ccb13ded0169ee7bba7725f5a3d
derrick b <dbozich1@gmail.com>
2018-08-23 01:11:10 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
   has_many :listings, :foreign_key => 'host_id'
   has_many :reservations, :foreign_key => 'guest_id'
   has_many :reviews, :foreign_key => 'guest_id'
+  has_many :reservations, through: :listings
 
-  # has_many :reservations, through: :listings
+
 
   def trips
     # binding.pry
